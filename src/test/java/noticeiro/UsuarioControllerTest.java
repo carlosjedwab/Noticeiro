58974bbde02c830a9ab97bf71998b2fc268f79c1
paulosestini <paulosestini890@gmail.com>
2020-05-21 16:42:37 -0300

    Update nos testes

--- src/test/java/noticeiro/UsuarioControllerTest.java
+++ src/test/java/noticeiro/UsuarioControllerTest.java
@@ -1,10 +1,6 @@
 package noticeiro;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,13 +11,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import noticeiro.dao.UsuarioRepository;
-import noticeiro.model.Link;
 import noticeiro.service.AuthenticationService;
 import noticeiro.service.UsuarioService;
 
